7b73b4cbe546fd150a92f9fdc2e183048efa3ca2
bakedoatmeal <jane.anqi.cui@gmail.com>
2021-09-26 21:12:48 -0400

    fixed display, added display each

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const tipInput = document.querySelector('#input-tip')
 const peopleInput = document.querySelector('#input-people')
 const displayTip = document.querySelector('#display-tip')
 const displayTotal = document.querySelector('#display-total')
+const displayEachH1 = document.querySelector('#display-each-h1')
+const displayEachP = document.querySelector('#display-each')
 
 billInput.addEventListener('input', updateTip)
 
@@ -15,17 +17,28 @@ function updateTip() {
     const bill = parseFloat(billInput.value)
     const tipAmount = parseInt(tipInput.value)
     const people = parseInt(peopleInput.value)
-    tip = (bill*tipAmount/100)/people
+    tip = (bill*tipAmount/100)
     displayTip.innerHTML = tip.toFixed(2)
-    updateTotal()
+    updateTotal(bill, tipAmount, people)
 }
 
-function updateTotal() {
-    const bill = parseFloat(billInput.value)
-    const tip = parseInt(tipInput.value)
-    const people = parseInt(peopleInput.value)
-    total = (bill+bill*tip/100)/people
+function updateTotal(bill, tip, people) {
+    total = (bill+bill*tip/100)
     displayTotal.innerHTML = total.toFixed(2)
 
+    if (people > 1) {
+        displayEach(bill, tip, people)
+    }
+}
+
+function displayEach(bill, tip, people) {
+    each = (bill+bill*tip/100)/people
+    if (people > 1){
+        displayEachH1.innerHTML = "Each"
+        displayEachP.innerHTML = each.toFixed(2)
+    } else {
+        displayEachH1.innerHTML = ""
+        displayEachP.innerHTML = ""
+    }
 }
 
