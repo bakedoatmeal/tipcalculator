2c73f380ebcd5b215aad7b0ccd35697082eb6680
bakedoatmeal <jane.anqi.cui@gmail.com>
2021-09-26 20:53:52 -0400

    functions working

--- script.js
+++ script.js
@@ -11,10 +11,21 @@ tipInput.addEventListener('input',  updateTip)
 peopleInput.addEventListener('input',  updateTip)
 
 function updateTip() {
-    return
+    console.log("called")
+    const bill = parseFloat(billInput.value)
+    const tipAmount = parseInt(tipInput.value)
+    const people = parseInt(peopleInput.value)
+    tip = (bill*tipAmount/100)/people
+    displayTip.innerHTML = tip.toFixed(2)
+    updateTotal()
 }
 
 function updateTotal() {
-    return
+    const bill = parseFloat(billInput.value)
+    const tip = parseInt(tipInput.value)
+    const people = parseInt(peopleInput.value)
+    total = (bill+bill*tip/100)/people
+    displayTotal.innerHTML = total.toFixed(2)
+
 }
 
