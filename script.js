9ad0358ef6bbab161cd953b32d2e41625920d8c2
bakedoatmeal <jane.anqi.cui@gmail.com>
2021-09-26 20:52:21 -0400

    defined variables and added event listeners

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const peopleInput = document.querySelector('#input-people')
 const displayTip = document.querySelector('#display-tip')
 const displayTotal = document.querySelector('#display-total')
 
+billInput.addEventListener('input', updateTip)
+
+tipInput.addEventListener('input',  updateTip)
+
+peopleInput.addEventListener('input',  updateTip)
+
+function updateTip() {
+    return
+}
+
+function updateTotal() {
+    return
+}
+
